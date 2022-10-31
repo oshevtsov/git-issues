fe77ee45bbea60ebe378083a1a39c30525f50111
Oleksii Shevtsov <alex.shevtsov1988@gmail.com>
2022-10-31 17:21:24 +0100

    Add minimal logging

--- lib/issues/github_issues.ex
+++ lib/issues/github_issues.ex
@@ -1,11 +1,15 @@
 defmodule Issues.GithubIssues do
+  require Logger
+
   @user_agent [ {"User-agent", "Elixir dave@pragprog.com"} ]
-  @github_url Application.fetch_env!(:issues, :github_url)
+  @github_url Application.compile_env!(:issues, :github_url)
 
   @moduledoc """
   Fetches the list of issues from a GitHub repo.
   """
   def fetch(user, project) do
+    Logger.info("Fetching #{user}'s project #{project}")
+
     issues_url(user, project)
     |> HTTPoison.get(@user_agent)
     |> handle_response
@@ -16,6 +20,8 @@ defmodule Issues.GithubIssues do
   end
 
   def handle_response({_, %{status_code: status_code, body: body}}) do
+    Logger.info("Got response: status code = #{status_code}")
+    Logger.debug(fn  -> inspect(body) end)
     {check_for_error(status_code), Jason.decode!(body)}
   end
 
